3c0c25a9b0d9ce4c1fbec6bc5c23eb113cca6f93
Arjan Scherpenisse <arjan@scherpenisse.net>
2013-08-20 22:05:55 +0200

    Initial commit

---
+++ slides/slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+    // Slide settings
+  settings: {
+    title: 'Arduino Workshop',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['hku'],
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Arjan Scherpenisse',
+    company: 'Miraclethings',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@acscherp',
+    www: 'http://miraclethings.nl/',
+    github: 'http://github.com/arjan'
+  }]
+};
+
